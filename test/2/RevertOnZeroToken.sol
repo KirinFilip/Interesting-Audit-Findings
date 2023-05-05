660883cf00935953000088813a30a49da39fb4fe
KirinFilip <filip.kirin.zg1@gmail.com>
2023-05-05 20:31:36 +0200

    Token for POC

---
+++ test/2/RevertOnZeroToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+/// This contract is for demonstration purposes only !
+
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract RevertOnZeroToken is ERC20 {
+    constructor(uint256 _totalSupply) ERC20("RevertOnZero", "ROZ") {
+        _mint(msg.sender, _totalSupply);
+    }
+
+    function transfer(address _to, uint256 _amount) public override returns (bool) {
+        require(_amount != 0, "Zero Value Transfer");
+        return super.transferFrom(msg.sender, _to, _amount);
+    }
+}
