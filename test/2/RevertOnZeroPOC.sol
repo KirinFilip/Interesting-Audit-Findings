606b9a47f82f42c17743750c8b59f51781630a9a
KirinFilip <filip.kirin.zg1@gmail.com>
2023-05-07 07:46:27 +0200

    comments

--- test/2/RevertOnZeroPOC.sol
+++ test/2/RevertOnZeroPOC.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.19;
 
 import "forge-std/Test.sol";
-import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 import {RevertOnZeroToken} from "./RevertOnZeroToken.sol";
 import {DutchAuction} from "./DutchAuction.sol";
 
@@ -10,11 +10,14 @@ contract RevertOnZeroTest is Test {
     DutchAuction public auction;
     RevertOnZeroToken public token;
     ERC721 public nft;
+    uint256 totalSupply = 100 ether;
 
     function setUp() public {
         nft = new ERC721("NFT", "NFT");
-        token = new RevertOnZeroToken(100e18);
-        //  startingPrice, discountRate, duration, nft, token
+        token = new RevertOnZeroToken(totalSupply);
+
+        // new DutchAuction(startingPrice, discountRate, duration, nft, token)
+        // 4 seconds to get to price of 0
         auction = new DutchAuction(1e18, 25e16, 4, address(nft), address(token));
     }
 
